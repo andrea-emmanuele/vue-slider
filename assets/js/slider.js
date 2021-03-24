25ba7e6cae36e2cb043fd7097ce79cd33c5be81b
Andrea <git@andreaemmanuele.it>
2021-03-24 15:56:44 +0100

    Initial commit

---
+++ assets/js/slider.js
@@ -0,0 +1,20 @@
+var slider = new Vue({
+    el: "#slider",
+    data: {
+        i: 0,
+        images: [
+            "https://picsum.photos/id/30/800/500",
+            "https://picsum.photos/id/29/800/500",
+            "https://picsum.photos/id/16/800/500",
+            "https://picsum.photos/id/12/800/500"
+        ]
+    },
+    methods: {
+        prev: function () {
+
+        },
+        next: function () {
+            
+        }
+    }
+})
